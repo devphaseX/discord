2dcace1009d06ec654d1d0fb814ce1837f09fa55
Ayomide Lawal <ayomidelawal700@gmail.com>
2023-09-09 13:40:33 +0100

    resolve leaking unowned channels

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,17 +1,21 @@
 import { InitialModal } from '@/components/modals/initial-modal';
 import { initialProfile } from '@/lib/initial-profile';
 import { db } from '@/schema/db';
-import { members } from '@/schema/tables';
-import { eq } from 'drizzle-orm';
+import { members, profiles, servers } from '@/schema/tables';
+import { and, eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 
 const SetupPage = async () => {
   const profile = await initialProfile();
 
-  const server = await db.query.servers.findFirst({
-    where: eq(members.profileId, profile.id),
-    with: { serverMembers: true },
-  });
+  const [server] = await db
+    .select({
+      id: servers.id,
+      name: servers.name,
+    })
+    .from(members)
+    .innerJoin(servers, eq(servers.id, members.serverId))
+    .where(eq(members.profileId, profile.id));
 
   if (server) {
     return redirect(`/servers/${server.id}`);
